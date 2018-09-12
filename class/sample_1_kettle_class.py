d27b9d84b05148915e4f2d2e55f0518a901ee075
Rahul Srivastava <rahusr@gmail.com>
2018-09-12 19:02:21 +0530

    Kettele class

---
+++ class/sample_1_kettle_class.py
@@ -0,0 +1,48 @@
+#This is a class definition
+class Kettle(object):
+
+    def __init__(self,make,price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+    def swithc_on(self):
+        self.on = True
+
+#Main program starts from here
+kenwood = Kettle("Kenwood",200)
+print(kenwood.make)
+print(kenwood.price)
+
+kenwood.price = 500
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton",300)
+
+print("Models : {}={} , {}={} ".format(kenwood.make, kenwood.price,hamilton.make, hamilton.price))
+
+print("Models : {0.make}={0.price}, {1.make}={1.price} ".format(kenwood,hamilton))
+
+
+#Switch on the kettle
+print(hamilton.on)
+hamilton.swithc_on()
+print(hamilton.on)
+
+#Calling method from Class , with out creating an instance
+Kettle.swithc_on(kenwood)
+print(kenwood.on)
+
+#Objects can be modified in python (Unlike Java and C++)
+print("*"*80)
+kenwood.power = 150
+print(kenwood.power)
+
+"""
+Class : Template for creating objects . All objects created using the same class will have same characterstics
+Object : An instance of the class
+Intantiate : Creating an insance of a class
+Method : A functon defined in a class
+Attribute : A variable bound to an instance of a class
+self : Is a reference to the instance of the class
+"""
