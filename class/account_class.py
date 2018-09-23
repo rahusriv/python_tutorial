498c98e8045f0df1f8a5faf4e845c1e707a71639
Rahul Srivastava <rahusr@gmail.com>
2018-09-23 15:35:57 +0530

    Commiting customer class

--- class/account_class.py
+++ class/account_class.py
@@ -1,17 +1,18 @@
 class Account(object):
     #account_number
     #name
-    bank = "Icici"
+    bank = "ICICI"
     branch = "MG Road Branch"
     ifsi_code ="ICICI0001"
-    balance = 10000
 
-    def __init__(self,name,account_number):
+    def __init__(self,name,account_number,balance):
         self.name = name
         self.account_number = account_number
+        self.balance = balance
 
     def credit_money(self,amount):
         self.balance = self.balance + amount
+        print("Deposited {} in your account".format(amount))
 
     def debit_money(self,amount):
         if(self.balance - amount < 0):
@@ -20,10 +21,29 @@ class Account(object):
             self.balance = self.balance - amount
             print("Amout debited")
 
+class Customer:
+    """This class will habe three variables
+    name: string
+    adhaar_no: string
+    account : Instace/Object of Account Class
+    """
+    def __init__(self, name , adhaar_no, account_obj):
+        self.name = name
+        self.adhaar_no = adhaar_no
+        self.account = account_obj
+
+    def deposit(self, amount):
+        self.account.credit_money(amount)
+
+    def check_balance(self):
+        return self.account.balance
+
+
+
 
 if __name__=='__main__':
-    mukund_account = Account("Mukund", "10001")
-    rahul_account = Account("Rahul","10002")
+    mukund_account = Account("Mukund", "10001",10000)
+    rahul_account = Account("Rahul","10002",10000)
 
     print("Balance for {} is {} ".format(mukund_account.name, mukund_account.balance))
     print("Balance for {} is {} ".format(rahul_account.name, rahul_account.balance))
@@ -32,6 +52,16 @@ if __name__=='__main__':
     rahul_account.debit_money(200)
     print("Balance for {} is {} ".format(rahul_account.name, rahul_account.balance))
     rahul_account.debit_money(10000)
+
+    #mukund_account.bank = "HDFC"
+
+    print(mukund_account.__dict__)
+    print(rahul_account.__dict__)
+    print(Account.__dict__)
+
+    customer1 = Customer("mukund","1001",mukund_account)
+    customer1.deposit(50000)
+    print(customer1.check_balance())
 
 
 
