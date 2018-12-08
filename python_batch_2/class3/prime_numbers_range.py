3e69c0d20ee0ad9f4119598474879c963da74fe5
P B Mukund <mukund_nsit@yahoo.in>
2018-12-08 16:39:17 +0530

    Prime numbers to file

--- python_batch_2/class3/prime_numbers_range.py
+++ python_batch_2/class3/prime_numbers_range.py
@@ -1,6 +1,9 @@
+
+
 start = int(input("Enter starting number(>5) :"))
 end = int(input("Enter ending number(<100000) :"))
 count = 0
+output_string = ""
 for i in range(start,end+1):
     prime = 1
     for j in range(2,i-1):
@@ -9,6 +12,14 @@ for i in range(start,end+1):
             break
     if(prime ==1):
         count = count+1
-        print("This {} is prime".format(i))
+        if(count%10 ==0):
+            output_string = output_string + "\n"
+        output_string = output_string + "  "+str(i)
+        #print("This {} is prime".format(i))
     else:
         pass
+
+print(" Total {} prime numbers found. They are given below: ".format(count))
+print(output_string)
+file = open("prime_numbers_{}_{}.txt".format(start,end),"w")
+file.write(output_string)
