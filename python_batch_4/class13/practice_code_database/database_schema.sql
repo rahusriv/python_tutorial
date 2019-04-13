adec23bc8c41f37a3154609b16371ba1be7cc302
Rahul Srivastava <rahusr@gmail.com>
2019-04-13 16:25:00 +0530

    Db schema

---
+++ python_batch_4/class13/practice_code_database/database_schema.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE ecomm;
+
+USE ecomm;
+
+CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255),phone_no VARCHAR(255),pwd VARCHAR(255), data VARCHAR(255), PRIMARY KEY (phone_no));
